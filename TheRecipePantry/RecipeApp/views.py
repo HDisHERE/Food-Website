4c97c024261adfc4d6eaba276d12fc25b8c8ffe3
kerosenee <124936213+kerosenee@users.noreply.github.com>
2024-02-29 09:11:51 -0500

    added urls to the navigation bar/get redirected to another html page

--- TheRecipePantry/RecipeApp/views.py
+++ TheRecipePantry/RecipeApp/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def ourgoal(request):
+    return render(request, 'ourgoal.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def questionnaire(request):
+    return render(request, 'questionnaire.html')
