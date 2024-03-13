33b1c77bc0b4069d5cb453eae00b0f203cbd3d7e
Irina <irina.kozhevnikov@gmail.com>
2024-03-13 17:53:39 -0400

    created recipe db model

--- TheRecipePantry/RecipeApp/models.py
+++ TheRecipePantry/RecipeApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    link = models.CharField(max_length=200)
+    cuisine = models.CharField(max_length=100)
+    diet = models.CharField(max_length=100)
+    prep_tim = models.TimeField(auto_now=False, auto_now_add=False)
+    keywords = models.CharField(max_length=300)
